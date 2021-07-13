1ae8090d3179ebc190f4c61b810786fdf77c7153
Hac Hai Pham <hpham.ux@gmail.com>
2021-07-13 20:44:25 +0200

    added player factories

--- logic.js
+++ logic.js
@@ -4,3 +4,21 @@ const gameBoard = [
   ['4','5','6'],
   ['7','8','9'],
 ];
+
+
+
+const playerFactory = (name => {
+    return {name}; //return an object
+  });
+
+const computerFactory = () => {
+    const computer = 'Computer';
+    return {computer};
+  };
+  
+  const player = playerFactory('Player');
+  const computer = computerFactory();
+  
+  console.log(player1.name); // 'jeff'
+  
+  jeff.sayHello(); // calls the function and logs 'hello!'
